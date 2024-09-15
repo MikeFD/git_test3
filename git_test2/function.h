fd769badc1c6dcd1e5a829260ec805604b7648a9
GreenT <limikefd@hotmail.com>
2024-09-15 13:41:43 +0800

    add function.h

---
+++ git_test2/function.h
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+using ElemType = int;
+
+/*
+函数功能：两数加法
+负责人：灰机
+功能：完成a + b的操作
+*/
+ElemType add(ElemType a, ElemType b);
+
+/*
+函数功能：两数减法
+负责人：灵泽
+功能：完成a - b的操作
+*/
+ElemType minus(ElemType a, ElemType b);
+
+/*
+函数功能：两数乘法
+负责人：飞
+功能：完成a x b的操作
+*/
+ElemType times(ElemType a, ElemType b);
+
+/*
+函数功能：两数除法
+负责人：崇
+功能： 完成a / b的操作
+*/
+ElemType divide(ElemType a, ElemType b);
+
+/*
+函数功能：三数加法
+负责人；小逸
+功能：完成a + b + c的操作
+*/
+ElemType triple_add(ElemType a, ElemType b, ElemType c);
+
+/*
+函数功能：三数减法
+负责人：faivm
+功能：完成a - b - c的操作
+*/
+ElemType triple_minus(ElemType a, ElemType b, ElemType c);
+
+
+
